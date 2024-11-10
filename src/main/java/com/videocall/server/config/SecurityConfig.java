aa77d201ed07067d3596fb2bba85f927772b285e
Tran Hoang Quan <quan02042004@gmail.com>
2024-11-10 13:37:07 +0700

    :bug:fix: add credential

--- src/main/java/com/videocall/server/config/SecurityConfig.java
+++ src/main/java/com/videocall/server/config/SecurityConfig.java
@@ -75,6 +75,7 @@ public class SecurityConfig {
 
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
